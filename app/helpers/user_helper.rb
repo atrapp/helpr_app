e7e5d2cdbf3ea98cd13ab3d7f67aefceb8611fb4
atrapp <andrea.trapp@gmx.net>
2014-05-21 12:01:30 -0400

    Gravatar working

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,10 +1,10 @@
 module UserHelper
   #Returns the Gravatar for the given user.
+  #default size is 80px x 80px
     def gravatar_for(user, options = { size: 100})      
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        size = options[:size]
-        # gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
+        size = options[:size]        
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")        
     end
 end
